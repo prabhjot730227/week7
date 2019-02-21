8fb8becf5d6e9daf063632d35aba7d7533501790
Prabhjot kaur <prabhjot7brar@gmail.com>
2019-02-21 14:55:20 -0500

    commit

--- week7/Program.cs
+++ week7/Program.cs
@@ -10,6 +10,27 @@ namespace week7
     {
         static void Main(string[] args)
         {
+            new PlayWithRecursion().Add(3, 4);
+            var a = new PlayWithRecursion();
+            Console.WriteLine(PlayWithRecursion.GetFactorial(4));
+        }
+    }
+    class PlayWithRecursion
+    {
+        public void Add(int a, int b)
+        {
+            if (a> 10) { return; }
+
+            Add(++a, --b);
+
+        }
+        public static long GetFactorial(int number)
+        {
+            Console.WriteLine(number);
+            if (number == 0) { return 1; }
+
+            return number * GetFactorial(number - 1);
+
         }
     }
 }
